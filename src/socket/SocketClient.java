7f27c958b14eeda7c94d3b1b0bdb04cf9dfa2755
kjwook7522 <kjwook7522@gmail.com>
2025-10-07 14:04:34 +0900

    feat: 소켓 통신 예제

---
+++ src/socket/SocketClient.java
@@ -0,0 +1,50 @@
+package socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SocketClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 8080);
+        System.out.println("Successfuly connect to server on 8080 port");
+
+        // 메세지 받기 쓰레드 시작
+        new Thread(new MessageHandler(socket)).start();
+
+        // 사용자가 입력한 메시지를 서버로 보내기
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        while ((input = userInput.readLine()) != null) {
+            out.println(input);
+        }
+
+        socket.close();
+    }
+
+    public static class MessageHandler implements Runnable {
+        private Socket socket;
+        private BufferedReader in;
+
+        public MessageHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                String message;
+                while ((message = in.readLine()) != null) {
+                    System.out.println(message);
+                }
+            } catch (IOException e) {
+                System.out.println("Connection closed");
+            }
+        }
+    }
+}
---
+++ src/socket/SocketServer.java
@@ -0,0 +1,86 @@
+package socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+public class SocketServer {
+    public static List<PrintWriter> clientOutputs = new ArrayList<>();
+    public static volatile boolean running = true;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("Start Server on 8080 port!");
+        System.out.println("Wating for client...");
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            running = false;
+
+            synchronized (clientOutputs) {
+                for (PrintWriter out : clientOutputs) {
+                    out.close();
+                }
+            }
+        }));
+
+        while (running) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("New Connection is created: " + clientSocket.getLocalAddress());
+            new Thread(new ClientHandler(clientSocket)).start();
+        }
+
+        serverSocket.close();
+        System.out.println("Server is closed");
+    }
+
+    public static class ClientHandler implements Runnable {
+        private Socket socket;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                synchronized (clientOutputs) {
+                    clientOutputs.add(out);
+                }
+
+                String message;
+                while ((message = in.readLine()) != null) {
+                    System.out.println("recv message: " + message);
+                    broadcast(message);
+                }
+            } catch (IOException e) {
+                System.out.println("Closed to client connection");
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+
+                synchronized (clientOutputs) {
+                    clientOutputs.remove(out);
+                }
+            }
+        }
+
+        private void broadcast(String message) {
+            synchronized (clientOutputs) {
+                for (PrintWriter writer : clientOutputs) {
+                    writer.println(message);
+                }
+            }
+        }
+    }
+}
